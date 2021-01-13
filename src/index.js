b374ba176ceded25d53fab55f33507bc332cf36b
mehdibiomally <lafdalimehdi0@gmail.com>
2021-01-13 17:06:28 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+import 'bootstrap'; //import 'bootstrap/dist/js/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
